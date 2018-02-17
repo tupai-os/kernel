682755257a5c7de3e6b60214375e2eb2b31a46cd
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-02-17 22:56:47 +0000

    Moved generic x86 boot code to common directory

--- src/arch/x86/i386/high.s
+++ src/arch/x86/i386/high.s
@@ -1,4 +1,4 @@
-// file : start.s
+// file : high.s
 //
 // Copyright (C) 2018  Joshua Barretto <joshua.s.barretto@gmail.com>
 //
@@ -16,64 +16,51 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 .extern _vga_print.boot
-.extern _check.boot
-.extern _paging_init.boot
-.extern _paging_enable.boot
+.extern kmain
 
-.global _start.boot
-.global _hang.boot
+.global _start
+.global _hang
 
 .set VIRTUAL_OFFSET, 0xC0000000
 
-.section .rodata.boot
-	_boot_msg:
-		.ascii "[INFO] Starting early kernel...\n\0"
-	_highjump_msg:
-		.ascii "[INFO] Jumping to high memory...\n\0"
+.section .rodata
+	_landed_msg:
+		.ascii "[ OK ] Landed in high memory\n\0"
+	_kmain_msg:
+		.ascii "[INFO] Starting kernel environment...\n\0"
 
-.section .bss.boot
+.section .bss
 	.align 16
-	_stack_start.boot:
-		.skip 256 // 256B stack
-	_stack_end.boot:
+	_stack_start:
+		.skip 4096 // 4K stack
+	_stack_end:
 
-	_mb_magic.boot:
-		.long
-	_mb_header.boot:
-		.long
+.section .text
+	.type _start, @function
+	_start:
+		// Set the higher boot stack
+		mov $_stack_end, %esp
 
-.section .text.boot
-	.type _start.boot, @function
-	_start.boot:
-		// Set the initial boot stack
-		mov $_stack_end.boot, %esp
-
-		// Preserve Multiboot attributes
-		mov %ebx, (_mb_magic.boot)
-		mov %eax, (_mb_header.boot)
-
-		// Initial boot text
-		push $_boot_msg
+		// We've landed!
+		push $_landed_msg
 		call _vga_print.boot
 		add $4, %esp
-
-		// Ensure we're running on a supported system
-		call _check.boot
-
-		// Setup higher-half kernel paging
-		call _paging_init.boot
-		call _paging_enable.boot
 
 		// Kmain boot text
-		push $_highjump_msg
+		push $_kmain_msg
 		call _vga_print.boot
 		add $4, %esp
 
-		// Jump to higher memory
-		call _start
+		// Pass Multiboot header to kmain
+		mov (_mb_header.boot), %eax
+		add VIRTUAL_OFFSET, %eax
+		push %eax
+
+		// Enter the kernel main
+		call kmain
 
 	// Hang the kernel if we ever get this far
-	_hang.boot:
+	_hang:
 		cli
 		hlt
-		jmp _hang.boot
+		jmp _hang
